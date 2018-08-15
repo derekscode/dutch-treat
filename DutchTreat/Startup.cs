4867af92fc0d8fcf5750de7d06050d981facad03
Derek <derek.heeren@ecoatm.com>
2018-08-15 15:05:39 -0700

    halfway through mvc

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -21,13 +21,18 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
 
             app.UseMvc(cfg =>
             {
-                cfg.MapRoute("Default", 
-                    "{controller}/{action}/{id?}", 
-                    new { controller = "App", Action = "Index"} );
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
             });
 
         }
